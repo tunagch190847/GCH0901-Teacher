02069ec87454a5a853e72db33548ec4f708a3d88
DoQuocBinh <binhdq@fpt.edu.vn>
2021-11-23 11:59:56 +0700

    ok2

--- GettingStarted/app.js
+++ GettingStarted/app.js
@@ -1,15 +1,20 @@
 const express = require('express')
+const fs = require('fs')
+
 const app = express()
 
 app.get('/',(req,res)=>{
     res.statusCode = 200
     res.setHeader('Content-Type','text/html')
-    res.end('<h1>Hello world</h1>')
+    res.write('<h1>Home page</h1>')
+    res.write("<ul><li><a href='/about'>About</a></li><li><a href='/help'>Help</a></li><li>3</li></ul>")
 })
 app.get('/about',(req,res)=>{
     res.statusCode = 200
     res.setHeader('Content-Type','text/html')
     res.write('<h2>About Page</h2>')
+    const data = fs.readFileSync('hello.txt','utf-8')
+    res.write(data)
     res.end("<ul><li>1</li><li>2</li><li>3</li></ul>")
 })
 
