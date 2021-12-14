939c62597a09b9f7326b9dc31db94b351ec71ca4
DoQuocBinh <binhdq@fpt.edu.vn>
2021-12-14 09:13:35 +0700

    validation and view all

--- NodeAndMongo/app.js
+++ NodeAndMongo/app.js
@@ -9,16 +9,35 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.get('/insert',(req,res)=>{
     res.render('product')
+})
+
+app.get('/view',async (req,res)=>{
+    //1. lay du lieu tu Mongo
+    const dbo = await getDatabase()
+    const results = await dbo.collection("Products").find({}).sort({name:1}).limit(7).toArray()
+    //2. hien thi du lieu qua HBS
+    res.render('view',{products:results})
 })
 
 app.post('/product',async (req,res)=>{
     const nameInput = req.body.txtName
     const priceInput = req.body.txtPrice
-    const newP = {name:nameInput,price:priceInput}
+    const picURLInput = req.body.txtPicURL
+    if(isNaN(priceInput)==true){
+        //Khong phai la so, bao loi, ket thuc ham
+        const errorMessage = "Gia phai la so!"
+        const oldValues = {name:nameInput,price:priceInput,picURL:picURLInput}
+        res.render('product',{error:errorMessage,oldValues:oldValues})
+        return;
+    }
+    const newP = {name:nameInput,price:Number.parseFloat(priceInput),picURL:picURLInput}
 
-    const client = await MongoClient.connect(DATABASE_URL);
-    const dbo = client.db(DATABASE_NAME);
+    const dbo = await getDatabase()
     const result = await dbo.collection("Products").insertOne(newP)
     console.log("Gia tri id moi duoc insert la: ", result.insertedId.toHexString());
     res.redirect('/')
@@ -27,3 +46,9 @@ app.post('/product',async (req,res)=>{
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
 console.log('Server is running!')
+
+async function getDatabase() {
+    const client = await MongoClient.connect(DATABASE_URL)
+    const dbo = client.db(DATABASE_NAME)
+    return dbo
+}
